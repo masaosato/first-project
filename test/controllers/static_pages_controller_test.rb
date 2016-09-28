c002bca4ae016e2b28f8148b49e655b231ade0f4
Masao <masao.sato@movaeic.jp>
2016-09-28 21:06:09 +0900

    finissh static-new

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
-    get static_pages_home_url
+    get :home
     assert_response :success
+    assert_select "title", "Home | Relaxy"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get :help
     assert_response :success
+    assert_select "title", "Help | Relaxy"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Relaxy"
+  end
 end
